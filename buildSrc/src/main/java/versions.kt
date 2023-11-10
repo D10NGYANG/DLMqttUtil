d5e68ca33e70520ab817fc9c03e0014ce7babe3f
D10NGYANG <D10NGYANG@163.com>
2023-11-10 18:13:45 +0800

    feat: update kotlin 1.9.20, compose compiler 1.5.4；

--- buildSrc/src/main/java/versions.kt
+++ buildSrc/src/main/java/versions.kt
@@ -4,10 +4,10 @@ object Project {
     const val target_sdk = 30
 }
 
-const val android_build_ver = "8.1.1"
-const val compose_ver = "androidx.compose:compose-bom:2023.08.00"
-const val compose_compiler_ver = "1.5.3"
-const val kotlin_ver = "1.9.10"
+const val android_build_ver = "8.1.3"
+const val compose_ver = "androidx.compose:compose-bom:2023.10.01"
+const val compose_compiler_ver = "1.5.4"
+const val kotlin_ver = "1.9.20"
 const val kotlin_coroutines_ver = "1.7.3"
 const val accompanist_ver = "0.32.0"
-const val jetpack_lifecycle_ver = "2.6.1"
+const val jetpack_lifecycle_ver = "2.6.2"
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.D10NGYANG"
-version = "0.0.2"
+version = "0.0.3"
 
 android {
     namespace = "com.d10ng.mqtt"
